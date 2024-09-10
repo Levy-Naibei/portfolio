c63be2e489e9e3fdebf8f5ad3d3b653895bb4d94
Levy-Naibei <levynaibei2@gmail.com>
2024-09-10 13:36:27 +0300

    chore: added more skills

--- src/components/about/Techstack.jsx
+++ src/components/about/Techstack.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { DiJavascript1, DiReact, DiNodejs, DiMongodb, DiPython, DiGit } from "react-icons/di";
-import { SiTypescript, SiFirebase, SiNextdotjs } from "react-icons/si";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPython,
+  DiDjango,
+  DiPostgresql,
+} from "react-icons/di";
+import {
+  SiTypescript,
+  SiFirebase,
+  SiNextdotjs,
+  SiGraphql,
+  SiRedux,
+} from "react-icons/si";
 
 const Techstack = () => {
   return (
@@ -16,6 +30,12 @@ const Techstack = () => {
         <DiReact />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <SiRedux />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiDjango />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
@@ -25,16 +45,19 @@ const Techstack = () => {
         <DiMongodb />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <DiPostgresql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
         <SiNextdotjs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
+        <SiGraphql />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiFirebase />
       </Col>
     </Row>
   );
-}
+};
 
 export default Techstack;
